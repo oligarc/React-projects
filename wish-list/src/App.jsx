b8b7be00b599f2de7f393f0c553bf2031e8e8a89
oligarc <oliver25399@gmail.com>
2025-01-15 12:43:21 +0100

    v1.0 but with htmlFor for labels

--- wish-list/src/App.jsx
+++ wish-list/src/App.jsx
@@ -28,9 +28,14 @@ function App() {
       </fieldset>
       <ul className='wish-list'>
         {wishes.map(wish => (
-          <li key={wish.id} className={`wish-list__item ${wish.completed ? 'wish-list__item--done' : ''}`}>
-            <input type="checkbox" checked={wish.completed} readOnly />
-            <label>{wish.text}</label>
+          <li key={wish.id} 
+          className={`wish-list__item 
+          ${wish.completed ? 'wish-list__item--done' : ''}`}>
+            <input type="checkbox" 
+            checked={wish.completed} 
+            readOnly />
+            <label htmlFor={wish.id} >
+              {wish.text}</label>
           </li>
         ))}
       </ul>
